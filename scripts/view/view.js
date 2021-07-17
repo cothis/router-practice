c0760626f3930a8a1385bc2505f3a53c3777f894
cothis <minho1030@naver.com>
2021-07-17 22:06:35 +0900

    Fix : SideView button 이벤트 element에서 찾아서 걸도록  수정

--- scripts/view/view.js
+++ scripts/view/view.js
@@ -79,7 +79,7 @@ export class SideView extends View {
 
   render() {
     this.slideView('up', 'down');
-    this.container.querySelector('#button').addEventListener('click', (e) => {
+    this.element.querySelector('#button').addEventListener('click', (e) => {
       RouterEvent.dispatchEvent('/main', true);
     });
   }

9249c24bfcb5151146fe5555b9cd1adda5b4cdfa
cothis <minho1030@naver.com>
2021-07-17 22:08:07 +0900

    Fix : SideView 사라지는거 오른쪽으로 수정

--- scripts/view/view.js
+++ scripts/view/view.js
@@ -78,7 +78,7 @@ export class SideView extends View {
   }
 
   render() {
-    this.slideView('up', 'down');
+    this.slideView('up', 'right');
     this.element.querySelector('#button').addEventListener('click', (e) => {
       RouterEvent.dispatchEvent('/main', true);
     });
